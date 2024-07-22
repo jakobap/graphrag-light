81cce681eb2d67099811e4a7c77087527fdfa18e
jakobap <jakobpoerschmann@gmail.com>
2024-07-22 20:16:56 +0200

    nosql kg dataclasses base definition

--- nosql-knowledgegraph/data_model.py
+++ nosql-knowledgegraph/data_model.py
@@ -1,19 +1,36 @@
 from dataclasses import dataclass
+from typing import List
 
+from networkx import edges
 
-@dataclass
-class NodeData:
-    name: str
-    description: str = None
 
 
 @dataclass
 class EdgeData:
-    name: str
-    description: str = None
+    destination_uid: str
+    target_uid: str 
+    description: str
+
+
+@dataclass
+class NodeData:
+    document_id: str # identifier for source knowlede base document for this entity
+    node_uid: str
+    node_title: str
+    node_type: str
+    node_description: str
+    node_degree: int
+    community_id: int | None = None # community id based on source document 
+    edges_to: List[EdgeData] = []
+    edges_from: List[str] = [] # in case of directed graph
+    embedding: List[float] = [] # text embedding representing node e.g. combination of title & description
 
 
 @dataclass
 class CommunityData:
-    name: str
-    description: str = None
+    document_id: str # identifier for source knowlede base document for this entity
+    community_uid: str # community identifier
+    community_nodes: List[str] # list of node_uid belonging to community
+    community_title: str | None = None # title of comm, None if not yet computed
+    community_description: str | None = None # description of comm, None if not yet computed
+    community_embedding: List[float] = [] # text embedding representing community
