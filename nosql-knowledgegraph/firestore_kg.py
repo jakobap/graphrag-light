c25d80733feb1a679170fbeceec626a39807104b
jakobap <jakobpoerschmann@gmail.com>
2024-07-26 15:37:38 +0200

    passing tests with firestore kg

--- nosql-knowledgegraph/firestore_kg.py
+++ nosql-knowledgegraph/firestore_kg.py
@@ -1,3 +1,4 @@
+from matplotlib.pylab import source
 from graph2nosql import NoSQLKnowledgeGraph
 from data_model import NodeData, EdgeData, CommunityData
 
@@ -14,7 +15,9 @@ class FirestoreKG(NoSQLKnowledgeGraph):
                  gcp_credential_file: str,
                  firestore_db_id: str,
                  node_collection_id: str,
-                 community_collection_id: str) -> None:
+                 edges_collection_id: str,
+                 community_collection_id: str
+                 ) -> None:
         """
         Initializes the FirestoreKG object.
 
@@ -27,7 +30,7 @@ class FirestoreKG(NoSQLKnowledgeGraph):
 
         if not firebase_admin._apps:
             credentials = firebase_admin.credentials.Certificate(
-                firestore_credential_file
+                gcp_credential_file
             )
             app = firebase_admin.initialize_app(credentials)
 
@@ -42,6 +45,7 @@ class FirestoreKG(NoSQLKnowledgeGraph):
         self.gcp_project_id = gcp_project_id
         self.database_id = firestore_db_id
         self.node_coll_id = node_collection_id
+        self.edges_coll_id = edges_collection_id
         self.community_coll_id = community_collection_id
 
     def add_node(self, node_uid: str, node_data: NodeData) -> None:
@@ -168,29 +172,194 @@ class FirestoreKG(NoSQLKnowledgeGraph):
         # 4. Finally, remove the node itself
         doc_ref.delete()
 
-    def add_edge(self, source_uid: str, target_uid: str, edge_data: EdgeData) -> None:
-        """Adds an edge (relationship) between two entities in the knowledge graph."""
-        pass
+    def add_edge(self, edge_data: EdgeData, directed: bool = True) -> None:
+        """
+        Adds an edge (relationship) between two entities in the knowledge graph.
+
+        Args:
+            source_uid (str): The UID of the source node.
+            target_uid (str): The UID of the target node.
+            edge_data (EdgeData): The edge data to be added.
+            directed (bool, optional): Whether the edge is directed. Defaults to True.
+        """
+
+        # Check if source and target nodes exist
+        if not self.get_node(edge_data.source_uid):
+            raise KeyError(f"Error: Source node with node_uid '{edge_data.source_uid}' does not exist.")
+        if not self.get_node(edge_data.target_uid):
+            raise KeyError(f"Error: Target node with node_uid '{edge_data.target_uid}' does not exist.")
+
+        # Type checking for edge_data
+        if not isinstance(edge_data, EdgeData):
+            raise TypeError(f"Error: edge_data must be of type EdgeData, not {type(edge_data)}")
+
+        edge_uid = self._generate_edge_uid(edge_data.source_uid, edge_data.target_uid)
+
+        try:
+            source_node_data = self.get_node(edge_data.source_uid)
+            target_node_data = self.get_node(edge_data.target_uid)
+
+            source_node_data.edges_to = tuple(set(source_node_data.edges_to) | {edge_data.target_uid})
+            self.update_node(edge_data.source_uid, source_node_data)
+
+            # Add the edge to the target node's edges_from
+            target_node_data.edges_from = tuple(set(target_node_data.edges_from) | {edge_data.source_uid})
+            self.update_node(edge_data.target_uid, target_node_data)
+
+            # Add the edge to the edges collection
+            self._update_egde_coll(edge_uid=edge_uid,
+                       target_uid=edge_data.target_uid,
+                       source_uid=edge_data.source_uid,
+                       description=edge_data.description)
+
+            if not directed:  # If undirected, add the reverse edge as well
+                target_node_data.edges_to = tuple(set(target_node_data.edges_to) | {edge_data.source_uid})
+                self.update_node(edge_data.target_uid, target_node_data)
+
+                # Since it's undirected, also add source_uid to target_node_data.edges_from
+                source_node_data.edges_from = tuple(set(source_node_data.edges_from) | {edge_data.target_uid})
+                self.update_node(edge_data.source_uid, source_node_data)
+
+                # Add the reverse edge to the edges collection
+                self._update_egde_coll(edge_uid=edge_uid,
+                                       target_uid=edge_data.source_uid,
+                                       source_uid=edge_data.target_uid,
+                                       description=edge_data.description)
+                                       
+
+        except ValueError as e:
+            raise ValueError(
+                f"Error: Could not add edge from '{edge_data.source_uid}' to '{edge_data.target_uid}'. Details: {e}"
+            ) from e
     
-    def get_edge(self, source_uid: str, target_uid: str, relationship: str) -> EdgeData:
-        """Retrieves an edge between two entities."""
-        pass
+    def get_edge(self, source_uid: str, target_uid: str) -> EdgeData:
+        """Retrieves an edge between two entities from the edges collection."""
+        edge_uid = self._generate_edge_uid(source_uid, target_uid)
+        edge_doc_ref = self.db.collection(self.edges_coll_id).document(edge_uid)
+        doc_snapshot = edge_doc_ref.get()
 
-    def update_edge(self, source_uid: str, target_uid: str, edge_data: EdgeData) -> None:
+        if doc_snapshot.exists:
+            try:
+                edge_data = EdgeData(**doc_snapshot.to_dict())
+                return edge_data
+            except TypeError as e:
+                raise ValueError(
+                    f"Error: Data fetched for edge_uid '{edge_uid}' does not match the EdgeData format. Details: {e}"
+                ) from e
+        else:
+            raise KeyError(f"Error: No edge found with edge_uid: {edge_uid}")
+
+    def update_edge(self, edge_data: EdgeData) -> None:
         """Updates an existing edge in the knowledge graph."""
-        pass
 
-    def remove_edge(self, source_uid: str, target_uid: str) -> None:
-        """Removes an edge between two entities."""
-        pass
+        # 1. Validate input and check if the edge exists
+        if not isinstance(edge_data, EdgeData):
+            raise TypeError(f"Error: edge_data must be of type EdgeData, not {type(edge_data)}")
+
+        edge_uid = self._generate_edge_uid(edge_data.source_uid, edge_data.target_uid)
+        
+        if not self.db.collection(self.edges_coll_id).document(edge_uid).get().exists:
+            raise KeyError(f"Error: Edge with edge_uid '{edge_uid}' does not exist.")
+
+        # 2. Update the edge document in the EDGES collection
+        try:
+            self._update_egde_coll(
+                edge_uid=edge_uid,
+                target_uid=edge_data.target_uid,
+                source_uid=edge_data.source_uid,
+                description=edge_data.description
+            )
+        except Exception as e:
+            raise Exception(f"Error updating edge in edges collection: {e}") from e
+
+        # 3. Update edge references in the NODES collection
+        try:
+            # 3a. Update source node
+            source_node_data = self.get_node(edge_data.source_uid)
+            # Ensure the target_uid is present in edges_to
+            if edge_data.target_uid not in source_node_data.edges_to:
+                source_node_data.edges_to = tuple(set(source_node_data.edges_to) | {edge_data.target_uid})
+                self.update_node(edge_data.source_uid, source_node_data)
+
+            # 3b. Update target node
+            target_node_data = self.get_node(edge_data.target_uid)
+            # Ensure the source_uid is present in edges_from
+            if edge_data.source_uid not in target_node_data.edges_from:
+                target_node_data.edges_from = tuple(set(target_node_data.edges_from) | {edge_data.source_uid})
+                self.update_node(edge_data.target_uid, target_node_data)
+
+        except Exception as e:
+            raise Exception(f"Error updating edge references in nodes: {e}") from e
+
+
+    def remove_edge(self, source_uid: str, target_uid: str, directed: bool = True) -> None:
+        """
+        Removes an edge between two entities in the knowledge graph. 
+        It tries to remove references from both node and edge collections.
+
+        Args:
+            edge_uid (str): The UID of the edge.
+            source_uid (str): The UID of the source node.
+            target_uid (str): The UID of the target node.
+            directed (bool, optional): Whether the edge is directed. Defaults to True.
+        """
+
+        edge_uid = self._generate_edge_uid(source_uid, target_uid)
+
+        try:
+            # --- 1. Remove edge references from the NODES collection ---
+            try:
+                source_node_data = self.get_node(source_uid)
+                source_node_data.edges_to = tuple(edge for edge in source_node_data.edges_to if edge != target_uid)
+                self.update_node(source_uid, source_node_data)
+
+                if not directed: 
+                    source_node_data.edges_from = tuple(edge for edge in source_node_data.edges_from if edge != target_uid)
+                    self.update_node(source_uid, source_node_data)
+            except KeyError:
+                # Source node not found, move on to the next step
+                pass 
+
+            try:
+                target_node_data = self.get_node(target_uid)
+                target_node_data.edges_from = tuple(edge for edge in target_node_data.edges_from if edge != source_uid)
+                self.update_node(target_uid, target_node_data)
+
+                if not directed: 
+                    target_node_data.edges_to = tuple(edge for edge in target_node_data.edges_to if edge != source_uid)
+                    self.update_node(target_uid, target_node_data)
+            except KeyError:
+                # Target node not found, move on to the next step
+                pass
+
+            # --- 2. Remove edge document from the EDGES collection ---
+            edge_doc_ref = self.db.collection(self.edges_coll_id).document(edge_uid)
+            edge_doc_ref.delete()
+
+        except Exception as e:
+            raise Exception(f"Error: Could not remove edge. Details: {e}") from e
 
     def list_communities(self, community_uid: str) -> List[CommunityData]:
         """Lists all communities for the given network."""
         pass
 
+    def _update_egde_coll(self, edge_uid: str, source_uid: str, target_uid: str, description: str) -> None:
+        """Update edge record in the edges collection."""
+        edge_doc_ref = self.db.collection(self.edges_coll_id).document(edge_uid)
+        edge_data_dict = {
+            "edge_uid": edge_uid,
+            "source_uid": source_uid,
+            "target_uid": target_uid,
+            "description": description
+        }
+        edge_doc_ref.set(edge_data_dict)    
+
     def _update_communities(self) -> None:
         """Computes Network communities and updates datastore respectively."""
         pass
+
+    def _generate_edge_uid(self, source_uid: str, target_uid: str):
+        return f"{source_uid}_to_{target_uid}"
 
 
 if __name__ == "__main__":
@@ -205,6 +374,7 @@ if __name__ == "__main__":
     project_id = str(secrets["GCP_PROJECT_ID"])
     database_id = str(secrets["FIRESTORE_DB_ID"])
     node_coll_id = str(secrets["NODE_COLL_ID"])
+    edges_coll_id = str(secrets["EDGES_COLL_ID"])
     community_coll_id = str(secrets["COMM_COLL_ID"])
 
     fskg = FirestoreKG(
@@ -212,6 +382,7 @@ if __name__ == "__main__":
         gcp_credential_file=firestore_credential_file,
         firestore_db_id=database_id,
         node_collection_id=node_coll_id,
+        edges_collection_id=edges_coll_id,
         community_collection_id=community_coll_id
     )
 
@@ -269,6 +440,5 @@ if __name__ == "__main__":
 
     # Get both nodes to deletion & corresponding edge update in remaining
     node_info = fskg.get_node(node_uid="test_node")
-    node_info = fskg.get_node(node_uid="test_node_add")
 
     print("Hello World!")
