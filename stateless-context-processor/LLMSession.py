dc455f6ef5e11f07ab1d2a3ac411a927f9f45c7b
jakobap <jakobpoerschmann@gmail.com>
2024-08-07 16:54:28 +0200

    json generation via function calling for flash

--- stateless-context-processor/LLMSession.py
+++ stateless-context-processor/LLMSession.py
@@ -12,12 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from cgi import test
 from optparse import Option
 from dotenv import dotenv_values
 import base64
 from google.cloud import aiplatform
 import vertexai
-from vertexai.generative_models import GenerativeModel, Part, FinishReason, GenerationConfig
+from vertexai.generative_models import GenerativeModel, Part, FinishReason, GenerationConfig, FunctionDeclaration, Tool, SafetySetting
 import vertexai.preview.generative_models as generative_models
 from vertexai.language_models import TextEmbeddingInput, TextEmbeddingModel
 import json
@@ -33,9 +34,30 @@ class LLMSession:
         self.model_name = model_name
         self.system_message = system_message
         self.secrets = dotenv_values(".env")
+        vertexai.init(
+            project=self.secrets["GCP_PROJECT_ID"], location=self.secrets["GCP_REGION"])
         self.model = GenerativeModel(
             self.model_name, system_instruction=[system_message])
         self.model_chat = self.model.start_chat()
+
+        self.safety_settings = [
+            SafetySetting(
+                category=SafetySetting.HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+                threshold=SafetySetting.HarmBlockThreshold.BLOCK_ONLY_HIGH
+            ),
+            SafetySetting(
+                category=SafetySetting.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+                threshold=SafetySetting.HarmBlockThreshold.BLOCK_ONLY_HIGH
+            ),
+            SafetySetting(
+                category=SafetySetting.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+                threshold=SafetySetting.HarmBlockThreshold.BLOCK_ONLY_HIGH
+            ),
+            SafetySetting(
+                category=SafetySetting.HarmCategory.HARM_CATEGORY_HARASSMENT,
+                threshold=SafetySetting.HarmBlockThreshold.BLOCK_ONLY_HIGH
+            )
+        ]
 
     @observe(as_type="generation")
     def generate(
@@ -57,6 +79,7 @@ class LLMSession:
                 response_mime_type=response_mime_type,
                 response_schema=response_schema
             ),
+            safety_settings=self.safety_settings,
             stream=False
         )
 
@@ -64,7 +87,8 @@ class LLMSession:
 
         self._langfuse_observation_meta(observation_name="Text Generate",
                                         query_string=client_query_string,
-                                        vertex_model_response=response)
+                                        vertex_model_response=response,
+                                        model_response_str=response_text)
 
         return response_text
 
@@ -88,6 +112,7 @@ class LLMSession:
         response = self.model_chat.send_message(
             client_query_string,
             stream=False,
+            safety_settings=self.safety_settings,
             generation_config=generation_config)
 
         text_response = response.text  # type: ignore
@@ -143,7 +168,8 @@ class LLMSession:
 
     def _langfuse_observation_meta(self, observation_name: str,
                                    query_string: str,
-                                   vertex_model_response) -> None:
+                                   vertex_model_response,
+                                   model_response_str: str) -> None:
         """
         Update langfuse observation with usage metadata.
         """
@@ -156,7 +182,7 @@ class LLMSession:
         langfuse_context.update_current_observation(
             name=observation_name,
             input=query_string,
-            output=vertex_model_response.text,
+            output=model_response_str,
             usage=ModelUsage(
                 unit="TOKENS",
                 input=input_token_count,
@@ -171,6 +197,62 @@ class LLMSession:
         )
         return None
 
+    @observe(as_type="generation")
+    def function_call_gen(
+        self,
+        client_query_string: str,
+        response_schema: Dict[str, Any],
+        max_output_tokens: int = 8192,
+        temperature: float = 0.0,
+        top_p: float = 0.3,
+    ) -> str:
+
+        function_decl = FunctionDeclaration(
+            name="extract_json_schema",
+            description="Record user question response and the estimated relevance score using well-structured JSON.",
+            parameters=response_schema
+        )
+
+        tools = Tool(function_declarations=[function_decl])
+
+        response = self.model.generate_content(
+            [client_query_string],
+            generation_config=GenerationConfig(
+                max_output_tokens=max_output_tokens,
+                temperature=temperature,
+                top_p=top_p,
+            ),
+            tools=[tools],
+            safety_settings=self.safety_settings,
+            stream=False
+        )
+
+        try:
+            response_text = response.text  # type: ignore
+
+            self._langfuse_observation_meta(observation_name="Function Call Text Generate",
+                                query_string=client_query_string,
+                                vertex_model_response=response, model_response_str=response_text)
+
+            response_schematic = json.loads(response_text)
+            print(f"No Function call done, parsed result: {response_schematic}")
+        except:
+            response_schematic = self._extract_arguments_from_model_response(response)
+
+            self._langfuse_observation_meta(observation_name="Function Call Text Generate",
+                                            query_string=client_query_string,
+                                            vertex_model_response=response, model_response_str=str(response_schematic))
+        
+        return str(response_schematic)
+
+    def _extract_arguments_from_model_response(self, model_response) -> dict:
+        """
+        Extract the raw function name and function calling arguments from the model response.
+        """
+        res = model_response.candidates[0].function_calls[0].args
+        func_arguments = {i: res[i] for i in res}
+        return func_arguments
+
 
 if __name__ == "__main__":
     response_schema = {
@@ -178,25 +260,39 @@ if __name__ == "__main__":
         "properties": {
             "response": {
                 "type": "string",
+                "description": "The response to the user question as raw string.",
             },
             "score": {
-                "type": "integer",
+                "type": "number",
+                "description": "The relevance score of the given community report context towards answering the user question [0.0, 10.0]",
             },
         },
         "required": ["response", "score"],
     }
 
-    llm = LLMSession(
-        system_message="Answer the following question truthfully and in json format. For every answer you generate you also need to generate a funnyness score to evaluate the fun factor of the question between 1 and 10.",
-        model_name="gemini-1.5-pro-001"
+    test_system = """Generate an answer to the following question truthfully. If you are unable to answer the question based on the given context respond 'Answer cannot be provided based on context'.
+                        Also generate a relevance score to evaluate the relevance of the provided community report context towards answering the user question between 0 and 10."""
+
+    # llm_pro = LLMSession(
+    #     system_message=test_system,
+    #     model_name="gemini-1.5-pro-001"
+    # )
+
+    llm_flash = LLMSession(
+        system_message=test_system,
+        model_name="gemini-1.5-flash-001"
     )
 
-    response = llm.generate(client_query_string="Which city has the most bridges?",
-                       response_schema=response_schema,
-                       response_mime_type="application/json")
-    
-    print(response)
+    # response = llm.generate(client_query_string="Which city has the most bridges?",
+    #                         response_schema=response_schema,
+    #                         response_mime_type="application/json")
+
+    # response_pro = llm_pro.function_call_gen(client_query_string="Which city has the most bridges?",
+                                    #  response_schema=response_schema)
+    # print(response_pro)
+
+    response_flash = llm_flash.function_call_gen(client_query_string="Which city has the most bridges?",
+                                     response_schema=response_schema)
+    print(response_flash)
 
     print("Hello World!")
-
-
