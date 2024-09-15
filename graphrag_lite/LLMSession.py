cf700c3291f2cfa71e077d653629b14f2f9e0424
jakobap <jakobpoerschmann@gmail.com>
2024-09-15 17:47:09 +0200

    graphrag_lite local install import correction

--- graphrag_lite/LLMSession.py
+++ graphrag_lite/LLMSession.py
@@ -12,10 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-try: 
-    import prompts
-except:
-    import graphrag.prompts as prompts
+import graphrag_lite.prompts as prompts
 
 from optparse import Option
 from dotenv import dotenv_values
