4b09c8142e36fd6d771712c39b319f90015f0792
jakobap <jakobpoerschmann@gmail.com>
2024-09-15 16:03:01 +0200

    requirements and setup.py for graphrag as own lib

---
+++ graphrag/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='graphrag-light',  # Choose a name for your library
+    version='0.1', 
+    packages=find_packages(),
+    install_requires=[
+        'google-cloud-aiplatform==1.59.0',
+        'google-cloud-documentai==2.29.2',
+        'google-auth==2.32.0',
+        'google-api-python-client==2.129.0',
+        'python-dotenv==1.0.1',
+        'networkx==3.3',
+        'matplotlib==3.9.1',
+        'langfuse==2.39.2',
+        'graspologic==3.4.1',
+        'google-cloud-pubsub==2.19.6',
+        'PyPDF2==3.0.1'
+    ]
+)
